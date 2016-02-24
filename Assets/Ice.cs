6d5c3dd705bef38cff84dc07b99925bb7c636dcf
CannibalK9 <ddaburns@gmail.com>
2016-02-24 18:11:13 +0000

    CharacterControllerRepo plus setup for ice collider

---
+++ Assets/Ice.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+
+public class Ice : MonoBehaviour
+{
+    public float DistanceAboveParent;
+    public float DistanceBetweenPoints;
+    public float RelativeCentre;
+    public float RelativeSize;
+
+	void Start ()
+    {
+        SetPositionToCentreOfAndDistanceAboveParent();
+        SetRelativeSize();
+        SetPolyColliderPoints();
+    }
+	
+    private void SetPositionToCentreOfAndDistanceAboveParent()
+    {
+        gameObject.transform.position = new Vector2(
+            gameObject.transform.parent.transform.position.x + RelativeCentre,
+            gameObject.transform.parent.transform.position.y + DistanceAboveParent);
+    }
+
+    private void SetRelativeSize()
+    {
+        gameObject.transform.localScale = new Vector2(RelativeSize, 1);
+    }
+
+    private void SetPolyColliderPoints()
+    {
+        var polyCollider = GetComponent<PolygonCollider2D>();
+
+        float boundaryWidth = polyCollider.bounds.size.x;
+        float relativeDistanceBetweenPoints = DistanceBetweenPoints / boundaryWidth;
+        int pointCount = Convert.ToInt32(1 / relativeDistanceBetweenPoints) + 3;
+
+        polyCollider.points = GetPointsEvenlySpacedOnNorthOfRectangle(relativeDistanceBetweenPoints, pointCount);
+    }
+
+    private Vector2[] GetPointsEvenlySpacedOnNorthOfRectangle(float northSideIncrement, int pointCount)
+    {
+        float max = 0.5f;
+        float min = -0.5f;
+        Vector2[] points = new Vector2[pointCount];
+
+        for (int i = 0; i < pointCount - 3; i++)
+        {
+            points[i] = new Vector2(min + northSideIncrement * i, max);
+        }
+
+        points[pointCount - 3] = new Vector2(max, max);
+        points[pointCount - 2] = new Vector2(max, min);
+        points[pointCount - 1] = new Vector2(min, min);
+
+        return points;
+    }
+
+    void Update ()
+    {
+	}
+}
