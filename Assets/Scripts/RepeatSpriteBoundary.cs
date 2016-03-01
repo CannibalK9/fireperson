07f5a114a64bc5db309acff538dadecb076b6e5a
CannibalK9 <ddaburns@gmail.com>
2016-03-01 07:54:03 +0000

    No more trigonometry!

--- Assets/Scripts/RepeatSpriteBoundary.cs
+++ Assets/Scripts/RepeatSpriteBoundary.cs
@@ -1,57 +1,57 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class RepeatSpriteBoundary : MonoBehaviour
 {
-    SpriteRenderer _sprite;
     void Awake()
     {
-        _sprite = GetComponent<SpriteRenderer>();
-        int spriteCount = transform.localScale.x > transform.localScale.y ? (int)transform.localScale.x : (int)transform.localScale.y;
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        float width = transform.localScale.x;
+        float height = transform.localScale.y;
 
-        Debug.Log(
-            _sprite.bounds.ToString());
-
-        float cosine = Mathf.Cos(transform.rotation.z);
-        Vector2 startPosition = new Vector2(
-            _sprite.bounds.min.x,
-            _sprite.bounds.min.y + cosine * transform.lossyScale.y/2);
-
-        Vector2 shiftValues = new Vector2(
-            cosine * transform.lossyScale.x / spriteCount,
-            Mathf.Sin(transform.rotation.z) * transform.lossyScale.y );
+        int spriteCount = width > height
+            ? (int)width
+            : (int)height;
 
         GameObject childPrefab = new GameObject();
 
         SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
-        childSprite.sprite = _sprite.sprite;
-        childSprite.sortingLayerID = _sprite.sortingLayerID;
-        childSprite.sortingOrder = _sprite.sortingOrder;
+        childSprite.sprite = sprite.sprite;
+        childSprite.sortingLayerID = sprite.sortingLayerID;
+        childSprite.sortingOrder = sprite.sortingOrder;
 
         GameObject child;
+        GameObject middleChild = new GameObject();
 
-        if (true)
+        for (int i = 0; i < spriteCount; i++)
         {
-            for (int i = 0; i < spriteCount; i++)
+            child = Instantiate(childPrefab) as GameObject;
+            child.transform.parent = middleChild.transform;
+            float iFloat = (float)Convert.ToDouble(i);
+            if (width > height)
             {
-                child = Instantiate(childPrefab) as GameObject;
-                child.transform.parent = transform;
-                child.transform.position = startPosition + i * shiftValues;
-                child.transform.localScale = new Vector2(1/ transform.localScale.x, 1);
-                child.transform.localRotation = new Quaternion();
+                child.transform.position = new Vector2(
+                    child.transform.position.x + (2f * iFloat - width + 1f) / (2f * width),
+                    child.transform.position.y);
+
+                child.transform.localScale = new Vector2(1 / width, 1);
+            }
+            else
+            {
+                child.transform.position = new Vector2(
+                    child.transform.position.x,
+                    child.transform.position.y + (2f * iFloat - height + 1f) / (2f * height));
+
+                child.transform.localScale = new Vector2(1, 1 / height);
             }
         }
-        else
-        {
-            for (int i = 0; i < spriteCount; i++)
-            {
-                child = Instantiate(childPrefab) as GameObject;
-                float offset = _sprite.bounds.size.x / 2 - 0.5f;
-                child.transform.parent = transform;
-            }
-        }
+        middleChild.transform.parent = transform;
+        middleChild.transform.localRotation = new Quaternion();
+        middleChild.transform.localScale = new Vector2(1, 1);
+        middleChild.transform.localPosition = new Vector2(0, 0);
 
         Destroy(childPrefab);
-        _sprite.enabled = false;
+        sprite.enabled = false;
     }
 }
